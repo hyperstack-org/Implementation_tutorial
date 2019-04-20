a19e964225d0464364db8dc44c6bcf0bdb8c8bbc
Barrie <barrie@tyger.com>
2019-04-20 06:23:44 +0100

    all working

--- sample/hello_world.rb
+++ sample/hello_world.rb
@@ -1,14 +1,15 @@
 class HelloWorld
   include Hyperstack::Component
-  param :a
 
-  render do
+  render(DIV) do
     H1 { 'Hello World from precompiled component' }
+    BUTTON { 'Click me' }.on(:click) { alert 'hello' }
   end
 end
 
 Document.ready? do
-  include Hyperstack::Component
-  Element['#site'].render { HelloWorld() }
-  # ReactAPI.render(ReactAPI.create_element(HelloWorld, {a: '1'}), Element['#site'])
+  include Hyperstack::Component # for ReactAPI
+  mount_point = Element['#site']
+  ReactAPI.render(ReactAPI.create_element(HelloWorld, {}), mount_point)
+  # mount_point.render
 end
